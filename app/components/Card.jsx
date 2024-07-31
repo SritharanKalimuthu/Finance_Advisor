ee928eb013e8b1be24c84e68bec81b9411cf7f3a
Levyathan <sritharkalimuthu@gmail.com>
2024-07-31 13:18:51 +0530

    Deploy Commit

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = () => {
   return (
     <div className='bg-zinc-950 p-8 text-white rounded-md mb-8'>
       <h1 className='text-xl font-semibold mb-4 uppercase'>Welcome to FinChat a NextJS AI</h1>
-      <p className='text-sm font-medium text-gray-400 leading-6'>Your personal finance assistant is here to help you manage your finances effectively. Ask me anything about budgeting, investments, savings, and more. Whether you&apos;re planning for retirement, looking to grow your investment portfolio, or simply trying to save more each month, I&apos;m here to provide personalized advice and guidance.<br></br> Let&apos;s work together to achieve your financial goals and build a secure future. Don&apos;t hesitate to ask for tips, strategies, and insights to make informed financial decisions. Together, we can navigate the complexities of finance with confidence.</p>
+      <p className='text-sm font-medium text-gray-400 leading-6'>Your personal finance assistant is here to help you manage your finances effectively. Ask me anything about budgeting, investments, savings, and more. Whether you`&apos;`re planning for retirement, looking to grow your investment portfolio, or simply trying to save more each month, I`&apos;`m here to provide personalized advice and guidance.<br></br> Let`&apos;`s work together to achieve your financial goals and build a secure future. Don`&apos;`t hesitate to ask for tips, strategies, and insights to make informed financial decisions. Together, we can navigate the complexities of finance with confidence.</p>
     </div>
   )
 }
