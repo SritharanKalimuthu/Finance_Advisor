17bb28ffc61bf06a74d1f2f4bf2a732ba4184860
Levyathan <sritharkalimuthu@gmail.com>
2024-07-31 13:51:00 +0530

    Deploy Commit

--- app/api/chat/[id].jsx
+++ app/api/chat/[id].jsx
@@ -8,11 +8,11 @@ const ChatMessage = ({ message, sender, image }) => {
   return (
     <section>
         <div className="flex items-start">
-          <Image src={image} alt="" width={30} className="bg-black p-2 rounded-full mr-5" />
-        <div>
-          <p className="uppercase text-slate-300 text-xs font-medium mb-2">{sender}</p>
-          <Markdown className="text-sm md:text-md font-medium leading-7">{message}</Markdown>
-        </div>
+          <Image src={image} alt="" width={25} className="bg-black p-2 rounded-full mr-2" />
+          <div className='mt-1'>
+            <p className="uppercase text-slate-300 text-xs font-medium mb-2">{sender}</p>
+            <Markdown className="text-sm md:text-md font-medium leading-7">{message}</Markdown>
+          </div>
       </div>
       <hr className="w-5/6 opacity-10 m-auto my-4"></hr>
     </section>
